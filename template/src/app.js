6b3dd37c1b4adfb3d7414da26caf5a8b106548b9
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2016-09-26 22:17:08 +0300

    chore: Better documenting theme file imports

--- template/src/app.js
+++ template/src/app.js
@@ -1,27 +1,20 @@
 import Vue from 'vue'
 import VueTouch from 'vue-touch'
-// import Vuex from 'vuex'
 // import VueResource from 'vue-resource'
 import Quasar from 'quasar'
 import Router from './router'
 
-/*
-  If overriding Quasar style, leave uncommented
-  just the first line.
-
-  If NOT overriding Quasar style, leave uncommented
-  just the second line. This option make compiling faster.
-
-  WARNING!
-  Leave just one of the two require() calls below
-  uncommented.
- */
+// === DEFAULT / CUSTOM STYLE ===
+// WARNING! always comment out ONE of the two require() calls below.
+// 1. use next line to activate CUSTOM STYLE (./src/themes)
 // require('./themes/app.' + __THEME + '.styl')
+// 2. or, use next line to activate DEFAULT QUASAR STYLE
 require('quasar/dist/quasar.' + __THEME + '.css')
+// ==============================
+
 Quasar.theme.set(__THEME)
 
 Vue.use(VueTouch) // Touch events
-// Vue.use(Vuex) // State Management
 // Vue.use(VueResource) // Ajax Requests
 Vue.use(Quasar) // Install Quasar Framework
 
